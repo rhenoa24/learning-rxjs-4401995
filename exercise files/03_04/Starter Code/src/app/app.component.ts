84d9cb3d6521fdca023cbbcd4e27f16e65110a98
Alyssa D. Bautista <alyssarhenoanicolebautista@gmail.com>
2026-06-10 18:04:40 +0800

    Starting subscriptions with first, last, and skipWhile (first, last are pipes)

--- exercise files/03_04/Starter Code/src/app/app.component.ts
+++ exercise files/03_04/Starter Code/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, last, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,14 @@ export class AppComponent {
     this.temperatureInputList.push(temperature);
   }
 
-  displayValues() {}
+  displayValues() {
+    this.temperatureSubject$ = from(this.temperatureInputList);
+    this.temperatureSubject$
+      .pipe(last())
+      .subscribe((temperature) => {
+        this.temperatureDataList.push(temperature)
+      });
+  }
 
   setInputTemperature(event: Event) {
     const input = (event.target as HTMLInputElement).value;
