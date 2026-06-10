418dfc775d29de0423779c62fd6df8a71faf8cf3
Alyssa D. Bautista <alyssarhenoanicolebautista@gmail.com>
2026-06-10 18:11:47 +0800

    Listening to multiple observables with combineLatestWith -> subscription will only update once both subjects were updated

--- exercise files/04_01/Starter Code/src/app/app.component.ts
+++ exercise files/04_01/Starter Code/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { combineLatestWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,20 @@ export class AppComponent implements OnInit {
   inputFeelsLikeTemperature = 0;
   displayText = '';
 
+  temperatureSubject$ = new Subject<number>();
+  feelsLikeSubject$ = new Subject<number>();
+
   ngOnInit() {
+    this.temperatureSubject$
+      .pipe(combineLatestWith(this.feelsLikeSubject$))
+      .subscribe(([temperature, feelsLikeTemperature]) => {
+        this.displayText = `It's ${temperature} F, but it feels like ${feelsLikeTemperature} F`
+      })
   }
 
   setTemperature() {
+    this.temperatureSubject$.next(this.inputTemperature);
+    this.feelsLikeSubject$.next(this.inputFeelsLikeTemperature);
   }
 
   setInputTemperature(event: Event) {
